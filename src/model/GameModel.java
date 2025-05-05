e7f8a6d6e8bd84fe6ea3ffac3cea9220d443ad10
Jonathan Liang <jonathan.liang3@gmail.com>
2025-05-05 13:16:28 -0400

    Update GameModel and GraphicalApp with improved ghost avoidance and AI behavior

--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -127,10 +127,10 @@ public class GameModel {
 
         actors = new ArrayList<>();
         // Uncomment this line after completing TODO 6
-        actors.add(new PacMannManual(this));
+        //actors.add(new PacMannManual(this));
         // TODO (challenge extension): Replace the above line with the following after completing
         //  the challenge extension
-        //actors.add(withAI ? new PacMannAI(this) : new PacMannManual(this));
+        actors.add(withAI ? new PacMannAI(this) : new PacMannManual(this));
         // Uncomment this line after completing TODO 13
         actors.add(new Blinky(this));
         // Uncomment this line after completing TODO 14
--- src/ui/GraphicalApp.java
+++ src/ui/GraphicalApp.java
@@ -14,7 +14,7 @@ public class GraphicalApp {
         // Default configuration parameters
         int width = 10;
         int height = 10;
-        boolean withAI = false;
+        boolean withAI = true;
         boolean showPaths = false;
         // Default to a different seed every time
         long seed = System.currentTimeMillis();
